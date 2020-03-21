a0521f009ac3a5688b18f3849cad2054af28434f
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-03-21 21:02:44 +0000

    Add basic progress bars with +/- buttons
    Far from the most efficient use of space and not very pretty either, but
    right now the progress bar is functional... providing you never refresh.

--- src/components/EventTasks.vue
+++ src/components/EventTasks.vue
@@ -2,12 +2,18 @@
   <ul>
     <li v-for="task in tasks" :key="task.description">
       {{ task.description }}
+      <task-progress :target="task.target" />
     </li>
   </ul>
 </template>
 
 <script>
+import TaskProgress from "./TaskProgress";
+
 export default {
+  components: {
+    TaskProgress
+  },
   props: {
     tasks: {
       type: Array,
---
+++ src/components/TaskProgress.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="progress">
+      <div
+        class="progress-bar"
+        role="progressbar"
+        :style="'width: ' + (current / target) * 100 + '%'"
+        :aria-valuenow="current"
+        :aria-valuemax="target"
+        aria-valuemin="0"
+      >
+        <span v-if="current / target >= 0.5">
+          {{ current }} of {{ target }}
+        </span>
+      </div>
+      <span v-if="current / target < 0.5">{{ current }} of {{ target }}</span>
+    </div>
+    <button @click="current--">-</button>
+    <button @click="current++">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    target: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      current: 0
+    };
+  }
+};
+</script>
