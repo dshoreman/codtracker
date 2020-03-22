16c6a871ec3f3ec9eaa58cc44d2a42103ebe7c44
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-03-22 19:14:43 +0000

    Add ability to toggle an input on progress bars
    This adds both an inner and outer number input with the value set to the
    current progress of that task. We're not using `v-model` here because if
    we were to break the 50% mark in any direction then it would switch, and
    that's not something we want as it loses focus on the input switched out.

--- src/components/TaskProgress.vue
+++ src/components/TaskProgress.vue
@@ -14,20 +14,53 @@
     </button>
     <div class="progress">
       <div
-        class="progress-bar bg-secondary"
+        :class="(readonly ? '' : 'focused ') + 'progress-bar bg-secondary'"
         role="progressbar"
         :style="'width: ' + (current / target) * 100 + '%'"
         :aria-valuenow="current"
         :aria-valuemax="target"
         aria-valuemin="0"
       >
-        <span v-if="current / target >= 0.5">
+        <template v-if="current / target >= 0.5">
+          <span
+            v-if="readonly"
+            @click="
+              readonly = false;
+              $nextTick(() => $refs.inner.focus());
+            "
+          >
+            {{ current }} of {{ target }}
+          </span>
+          <input
+            v-else
+            ref="inner"
+            type="number"
+            :value="current"
+            class="inner form-control-sm"
+            @keyup.enter="edit"
+          />
+        </template>
+      </div>
+      <template v-if="current / target < 0.5">
+        <span
+          v-if="readonly"
+          class="outer"
+          @click="
+            readonly = false;
+            $nextTick(() => $refs.outer.focus());
+          "
+        >
           {{ current }} of {{ target }}
         </span>
-      </div>
-      <span v-if="current / target < 0.5" class="outer">
-        {{ current }} of {{ target }}
-      </span>
+        <input
+          v-else
+          ref="outer"
+          type="number"
+          :value="current"
+          class="form-control-sm"
+          @keyup.enter="edit"
+        />
+      </template>
     </div>
   </div>
 </template>
@@ -42,6 +75,17 @@ export default {
     target: {
       type: Number,
       default: 0
+    }
+  },
+  data() {
+    return {
+      readonly: true
+    };
+  },
+  methods: {
+    edit($event) {
+      this.$emit("update:current", parseInt($event.target.value));
+      this.readonly = true;
     }
   }
 };
@@ -58,8 +102,23 @@ export default {
 .progress {
   height: 24px;
 }
+.progress-bar.focused {
+  background: #4a535b !important;
+  border: 2px solid rgba(0, 0, 0, 0.5);
+}
 span.outer {
   margin-left: 5px;
   margin-top: 2px;
+}
+input {
+  background-color: rgba(0, 0, 0, 0);
+  border: none;
+  height: 24px;
+  width: 100px;
+}
+input.inner {
+  color: #fff;
+  margin: 0 auto;
+  text-align: center;
 }
 </style>
